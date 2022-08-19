3ff938f8bd5b9f7e9538d18fa793bc8aeeb5068c
Saikumar <saikumar.sahukara@gmail.com>
2022-08-19 22:07:50 +0530

    object declaration

--- c3_s5_pc1_StudentsGrade/src/StudentsMainImpl.java
+++ c3_s5_pc1_StudentsGrade/src/StudentsMainImpl.java
@@ -1,5 +1,11 @@
+import com.marks.StudentMarks;
+
 public class StudentsMainImpl {
     public static void main(String[] args) {
+        StudentMarks studentMarks =new StudentMarks();
+        studentMarks.calculateMarks(35,36,39);
+        studentMarks.calculateMarks(41,49,80,95);
+        studentMarks.calculateMarks(23,36,45);
         
     }
 }
