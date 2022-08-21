84e9b2e67c26b35546bf0bc584454fb0b98eb66e
Saikumar <saikumar.sahukara@gmail.com>
2022-08-21 17:36:00 +0530

    final commit

--- c3_s5_pc1_StudentsGrade/src/StudentsMainImpl.java
+++ c3_s5_pc1_StudentsGrade/src/StudentsMainImpl.java
@@ -2,10 +2,12 @@ import com.marks.StudentMarks;
 
 public class StudentsMainImpl {
     public static void main(String[] args) {
-        StudentMarks studentMarks =new StudentMarks();
-        studentMarks.calculateMarks(35,36,39);
-        studentMarks.calculateMarks(41,49,80,95);
-        studentMarks.calculateMarks(23,36,45);
-
+        StudentMarks studentMarks = new StudentMarks();
+        System.out.println("studentMarks of Medical Studies = " + studentMarks.calculateMarks(97, 99, 78));
+        System.out.println("studentMarks of Non medical Studies = " + studentMarks.calculateMarks(99, 89, 89, 89));
+        System.out.println("studentMarks of Businness Studies = " + studentMarks.calculateMarks(24.5, 99, 99.5));
     }
 }
+
+
+
